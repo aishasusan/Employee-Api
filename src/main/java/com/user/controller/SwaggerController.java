65380b36ebbb3df1898e54e4f5ae771945307a49
AISHA SUSAN GREGORY <aishasusan2@gmail.com>
2021-06-27 17:38:02 +0530

    latest commits

--- src/main/java/com/user/controller/SwaggerController.java
+++ src/main/java/com/user/controller/SwaggerController.java
@@ -1,11 +1,10 @@
 package com.user.controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SwaggerController {
 	@GetMapping("/")
 	public String callSwaggerUI() {
